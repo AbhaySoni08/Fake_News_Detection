ef7a686d3f05202be28d1d55b73ad1e91c5a6cde
dev soni <soniabhay9547@gmail.com>
2023-10-02 21:33:13 +0530

    update code

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,33 @@
+from flask import Flask , request, render_template
+import pickle
+
+vector = pickle.load(open("vectorizer.pkl",'rb'))
+model = pickle.load(open("finalized_model.pkl",'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template("index.html")
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    if request.method == "POST":
+        news = str(request.form['news'])
+        print(news)
+
+        predict = model.predict(vector.transform([news]))
+        print(predict)
+
+        return render_template("prediction.html", prediction_text="News headline is-> {}" .format(predict))
+              
+    else:
+        return render_template("Prediction.html")
+
+if __name__== '_main_':
+    app.run()
+
+    
+
+
+   
